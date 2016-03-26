b8aedaa81f3bde7d0487ffb1db5dc95da9dff539
Nicollette <nicci.hydak@gmail.com>
2016-03-25 21:40:19 -0400

    addition of questions to javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-$(document).ready(function() {
-$('.start').click(function() {
-    $('.introduction').hide();
-    $('.start').hide();
-    $('.question1').show();
-    $('.submit').show();
-});	
-});
+var questions = [ {
+    question: "What is the name of the Golden Goddesses that created the land of Hyrule?",
+    answers: ["Saria, Zelda, and Impa","Din, Nayru, and Farore","Lily, Violet, and Rose","Annalise, Clara, and Josaphine"],
+    correctAnswer: "Saria, Zelda, and Impa"
+  }, 
+  {
+    question: "What are the three parts of the Triforce called?",
+    answers: ["Power, Wisdom, and Courage", "Bravery, Courage, and Strength","Power, Wisdom, and Bravery","Wisdom, Bravery, and Strength"],
+    correctAnswer: "Power, Wisdom, and Courage"
+  },
+  {
+    question: "Where do you first meet your horse, Epona?",
+    answers: ["Kokiri Forest","Gerudo Valley","Lon Lon Ranch","Goron City"],
+    correctAnswer: "Lon Lon Ranch"
+  }, 
+  {
+    question: "What temple serves as the entrance to the sacred realm?",
+    answers: ["Temple of Time","Shadow Temple","Fire Temple","Water Temple"],
+    correctAnswer: "Temple of Time"
+  }, 
+  {
+    question: "What song would you play to warp to the Forest Temple?",
+    choices: ["Forest Lullaby","Minuet of Forest","Bolero of Trees","Serenade of the Woods"]
+    correctAnswer: "Minuet of Forest"
+  }];
